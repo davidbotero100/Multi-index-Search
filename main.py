cbe3e368f13a6cbfbcab9386d4b0907058f332ea
davidbotero100 <dboterov@hotmail.com>
2024-09-21 11:04:51 -0700

    Better variable declarations.

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ class multiIndex():
     data = []
 
     #Dictionary of indecies
-    indecies = dict(i=0, j=0)
+    indecies = dict( i=0, j=0, k=0, m=0,
+                    l=0, n=0, p=0, o=0, )
 
     """
     i = size -1
@@ -45,7 +46,10 @@ class multiIndex():
 
         #print(self.numbers)
 
-        return self.numbers
+        # Convert str user input
+        self.converted_numbers = int(self.numbers)
+
+        return self.converted_numbers
                
 
     def fillList(self, numbers: int) -> None:
@@ -69,6 +73,7 @@ class multiIndex():
         for i in range(times):
             shuffle(self.data)
 
+    # For testing index updates
     def testIndecies(self) -> None:
         
         for i in range(5):
@@ -76,9 +81,21 @@ class multiIndex():
             self.indecies['j'] += 1
         print(f"After iteration {i + 1}: {self.indecies}")
 
+    def setIndecies(self, numbers: int) -> None:
 
-                
-               
+        list_size = numbers
+
+        self.indecies['i'] = 0
+
+        self.indecies['j'] = list_size//4
+        self.indecies['k'] = list_size//4+1
+
+        self.indecies['m'] = list_size//2
+        self.indecies['n'] = list_size//2+1
+
+        self.indecies['l'] = list_size
+
+
     
 if __name__ == '__main__':
         
@@ -89,10 +106,13 @@ if __name__ == '__main__':
 
     numbers = mi.getUserInput()
     
-    converted_int = int(numbers)
-    mi.fillList(converted_int)
-    mi.testIndecies()
-    
+    #converted_numbers = int(numbers)
+    mi.fillList(numbers)
+
+    # For testing only
+    # mi.testIndecies()
+
+    mi.setIndecies(numbers)
 
     t.toc()
 
